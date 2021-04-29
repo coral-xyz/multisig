ce6748f392e1ba6fbcb067671f6b326b11582bcf
Ruud van Asseldonk <ruud@chorus.one>
2021-04-29 15:57:49 +0200

    Accept owners in "multisig create-multisig"

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,7 +6,7 @@ use anchor_client::solana_sdk::signature::read_keypair_file;
 use anchor_client::solana_sdk::signature::{Keypair, Signer};
 use anchor_client::solana_sdk::system_instruction;
 use anchor_client::solana_sdk::sysvar;
-use anchor_client::{Client, Cluster, EventContext, Program};
+use anchor_client::{Client, Cluster, Program};
 use clap::Clap;
 use multisig::accounts as multisig_accounts;
 use multisig::instruction as multisig_instruction;
@@ -26,7 +26,17 @@ struct Opts {
 #[derive(Clap, Debug)]
 enum SubCommand {
     /// Create a new multisig address.
-    CreateMultisig
+    CreateMultisig(CreateMultisigOpts)
+}
+
+#[derive(Clap, Debug)]
+struct CreateMultisigOpts {
+    /// How many signatures are needed to approve a transaction.
+    #[clap(long)]
+    threshold: u64,
+
+    /// The public keys of the multisig owners, who can sign transactions.
+    owners: Vec<Pubkey>,
 }
 
 /// Read the keypair from ~/.config/solana/id.json.
@@ -48,11 +58,16 @@ fn main() {
     let program = client.program(opts.multisig_program_id);
 
     match opts.subcommand {
-        SubCommand::CreateMultisig => create_multisig(program),
+        SubCommand::CreateMultisig(cmd_opts) => create_multisig(program, cmd_opts),
     }
 }
 
-fn create_multisig(program: Program) {
+fn create_multisig(program: Program, opts: CreateMultisigOpts) {
+    if opts.threshold > opts.owners.len() as u64 {
+        println!("Threshold must be at most the number of owners.");
+        std::process::exit(1);
+    }
+
     // Before we can make the Multisig program initialize a new multisig
     // account, we need to have a program-owned account to used for that.
     // We generate a temporary key pair for this; after the account is
@@ -89,10 +104,8 @@ fn create_multisig(program: Program) {
             rent: sysvar::rent::ID,
         })
         .args(multisig_instruction::CreateMultisig {
-            owners: vec![
-                program.payer(),
-            ],
-            threshold: 1,
+            owners: opts.owners,
+            threshold: opts.threshold,
             nonce: 0,
         })
         .send()
