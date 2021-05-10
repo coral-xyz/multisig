af168430d17035ec5891e080388823541adffdd7
Ruud van Asseldonk <ruud@chorus.one>
2021-05-10 11:19:25 +0200

    Expose struct fields, add From impls
    This enables the structs to be uses from an external crate that is using
    "anchor_client". Without this the structs are opaque, and the client
    cannot do anything useful. The From impls make the types more convenient
    to use.

--- programs/multisig/src/lib.rs
+++ programs/multisig/src/lib.rs
@@ -230,35 +230,35 @@ pub struct ExecuteTransaction<'info> {
 
 #[account]
 pub struct Multisig {
-    owners: Vec<Pubkey>,
-    threshold: u64,
-    nonce: u8,
-    owner_set_seqno: u32,
+    pub owners: Vec<Pubkey>,
+    pub threshold: u64,
+    pub nonce: u8,
+    pub owner_set_seqno: u32,
 }
 
 #[account]
 pub struct Transaction {
     // The multisig account this transaction belongs to.
-    multisig: Pubkey,
+    pub multisig: Pubkey,
     // Target program to execute against.
-    program_id: Pubkey,
+    pub program_id: Pubkey,
     // Accounts requried for the transaction.
-    accounts: Vec<TransactionAccount>,
+    pub accounts: Vec<TransactionAccount>,
     // Instruction data for the transaction.
-    data: Vec<u8>,
+    pub data: Vec<u8>,
     // signers[index] is true iff multisig.owners[index] signed the transaction.
-    signers: Vec<bool>,
+    pub signers: Vec<bool>,
     // Boolean ensuring one time execution.
-    did_execute: bool,
+    pub did_execute: bool,
     // Owner set sequence number.
-    owner_set_seqno: u32,
+    pub owner_set_seqno: u32,
 }
 
 impl From<&Transaction> for Instruction {
     fn from(tx: &Transaction) -> Instruction {
         Instruction {
             program_id: tx.program_id,
-            accounts: tx.accounts.clone().into_iter().map(Into::into).collect(),
+            accounts: tx.accounts.iter().map(AccountMeta::from).collect(),
             data: tx.data.clone(),
         }
     }
@@ -266,16 +266,26 @@ impl From<&Transaction> for Instruction {
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct TransactionAccount {
-    pubkey: Pubkey,
-    is_signer: bool,
-    is_writable: bool,
+    pub pubkey: Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
 }
 
-impl From<TransactionAccount> for AccountMeta {
-    fn from(account: TransactionAccount) -> AccountMeta {
+impl From<&TransactionAccount> for AccountMeta {
+    fn from(account: &TransactionAccount) -> AccountMeta {
         match account.is_writable {
             false => AccountMeta::new_readonly(account.pubkey, account.is_signer),
             true => AccountMeta::new(account.pubkey, account.is_signer),
+        }
+    }
+}
+
+impl From<&AccountMeta> for TransactionAccount {
+    fn from(account_meta: &AccountMeta) -> TransactionAccount {
+        TransactionAccount {
+            pubkey: account_meta.pubkey,
+            is_signer: account_meta.is_signer,
+            is_writable: account_meta.is_writable,
         }
     }
 }
