22fd3e4a6f728f8f009f3470cdc2c0cfe63d16d1
armaniferrante <armaniferrante@gmail.com>
2021-09-04 05:00:15 -0700

    Update anchor v0.14.0

--- tests/multisig.js
+++ tests/multisig.js
@@ -5,7 +5,7 @@ describe("multisig", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.SerumMultisig;
+  const program = anchor.workspace.SerumMultisigImpl;
 
   it("Tests the multisig program", async () => {
     const multisig = anchor.web3.Keypair.generate();
