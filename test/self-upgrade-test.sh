1ad737014c893358754eff13cbe2d25c12e43f4b
Drew Nutter <dnut@users.noreply.github.com>
2022-04-29 17:33:29 -0400

    self upgrade test wip

--- test/self-upgrade-test.sh
+++ test/self-upgrade-test.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 DEFAULT_PROGRAM_ID=JPEngBKGXmLUWAXrqZ66zTUzXNBirh5Lkjpjh7dfbXV
 TEST_PROGRAM_ID=HG4RYfYAwnnXHpWH583VA1yFgBQq3n7y31ks5F2rpe2j
-
+OLD_BINARY=test/old_multisig-new-build.so
 
 main() {
     [ -f Anchor.toml ] \
@@ -10,65 +10,79 @@ main() {
             && exit 40)
     avm use 0.21.0
 
-    ==== deploy old multisig to localnet
+    ~# deploy old multisig to localnet
     start-localnet
+    solana -ul program deploy $OLD_BINARY --program-id test/program.json
+    enable-logging
 
-    ==== generate owners
+    ~# generate owners
     local owner1=$(keygen owner1.json)
     local owner2=$(keygen owner2.json)
     local owner3=$(keygen owner3.json)
     local delegate1=$(keygen delegate1.json)
     local unauthorized=$(keygen unauthorized.json)
 
-    ==== create a multisig with two owners and threshold = 2
+    ~# create a multisig with two owners and threshold = 2
     eval $(awk 'END{print \
         "local multisig=" $1 ";",\
         "local signer=" $2
     }'<<<$(old-multisig admin new 2 $owner1 $owner2))
 
-    ==== give upgrade authority for the multisig program to the multisig
+    ~# give upgrade authority for the multisig program to the multisig
     solana -ul program set-upgrade-authority $TEST_PROGRAM_ID --new-upgrade-authority $signer
 
-    ==== add a delegate for owner 1
+    ~# add a delegate for owner 1
     old-multisig -k $owner1 admin add-delegates $delegate1
 
-    ==== create proposal 1 to upgrade a verifiable build of the new multisig
+    ~# create proposal 1 to upgrade a verifiable build of the new multisig
     local proposal1=$(build-and-propose $owner1)
 
-    ==== approve proposal 1 with owners 2 and 3 and execute
+    ~# approve proposal 1 with owners 2 and 3 and execute
     old-multisig -k $owner2 approve $proposal
     old-multisig -k $owner3 approve $proposal
     old-multisig execute $proposal
 
-    ==== verify the upgrade
+    ~# verify the upgrade
     anchor verify $TEST_PROGRAM_ID
 
-    ==== create proposal 2 to rollback the multisig program to the old version
-    local proposal2=$(multisig -k $deployer propose program upgrade $TEST_PROGRAM_ID test/old_multisig.so)
+    ~# create proposal 2 to rollback the multisig program to the old version
+    local proposal2=$(multisig -k $deployer propose program upgrade $TEST_PROGRAM_ID $OLD_BINARY)
 
-    ==== approve with owner 3
+    ~# approve with owner 3
     new-multisig -k $owner3 approve $proposal2
 
-    ==== execution is not allowed
+    ~# execution is not allowed
     new-multisig execute $proposal2
 
-    ==== fail to vote on the proposal using invalid wallet
+    ~# fail to vote on the proposal using invalid wallet
     new-multisig -k $unauthorized approve $proposal2
 
-    ==== execution is not allowed
+    ~# execution is not allowed
     new-multisig execute $proposal2
 
-    ==== approve with delegate for owner 1
+    ~# approve with delegate for owner 1
     new-multisig -k $delegate1 approve $proposal2
     
-    ==== execute the proposal
+    ~# execute the proposal
     new-multisig execute $proposal2
 
-    ==== verify the upgrade
+    ~# verify the upgrade
     solana program dump JPEngBKGXmLUWAXrqZ66zTUzXNBirh5Lkjpjh7dfbXV dump.so
-    assert_eq $(md5sum test/old_multisig.so) $(md5sum dump.so) 'deployed multisig does not match old multisig after rollback'
+    assert_eq $(md5sum $OLD_BINARY) $(md5sum dump.so) 'deployed multisig does not match old multisig after rollback'
 
     clean_up
+}
+
+
+SOLANA_LOG_PID=
+
+enable-logging() {
+    solana -ul logs &
+    SOLANA_LOG_PID=$!
+}
+
+disable-logging() {
+    kill SOLANA_LOG_PID
 }
 
 replace-program-id() { local old=$1; local new=$1
@@ -91,15 +105,18 @@ old-multisig() {
 }
 
 start-localnet() {
-    solana-test-validator -r >/dev/null & #--bpf-program "$PROGRAM_ID" test/old_multisig.so
-    sleep 3
-    solana -ul logs &
+    solana-test-validator -r >/dev/null &
     trap "clean_up && trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+    sleep 5
 }
 
 build-and-propose() { local deployer=$1
+    sed "s/$DEFAULT_PROGRAM_ID/$TEST_PROGRAM_ID/g" programs/multisig/src/lib.rs
     anchor build --verifiable
+    sed "s/$TEST_PROGRAM_ID/$DEFAULT_PROGRAM_ID/g" programs/multisig/src/lib.rs
+    disable-logging
     solana -ul program write-buffer target/verifiable/multisig.so
+    enable-logging
     multisig -k $deployer propose program upgrade $TEST_PROGRAM_ID target/verifiable/multisig.so
 }
 
@@ -113,7 +130,7 @@ assert_eq() { local expected=$1; local actual=$2, local message=$3
 }
 
 clean_up() {
-    ==== cleaning up test artifacts
+    ~# cleaning up test artifacts
     rm dump.so
     rm owner1.json
     rm owner2.json
@@ -122,7 +139,7 @@ clean_up() {
     rm unauthorized.json
 }
 
-====() {
+~#() { # recognized as a comment by vscode but a command by bash -- perfect!
     set +x
     echo
     echo ================================================================================
